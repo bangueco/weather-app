9f8b88f712d58d3d867c6c78d3b153da31db8feb
ivandnd <121280104+ivandnd@users.noreply.github.com>
2024-02-01 23:09:44 +0800

    Add functionalities for getting, processing and displaying data

---
+++ src/modules/Logic.js
@@ -0,0 +1,33 @@
+import { getWeatherData } from './WeatherApi';
+import { weatherInfoDOM } from './DOM';
+
+function processData(data) {
+  const cityProcessedData = {
+    city_info: {
+      city_name: data.location.name,
+      city_country: data.location.country,
+      city_localtime: data.location.localtime,
+    },
+    current_forecast: {
+      temperature: data.current.temp_c,
+      wind: data.current.wind_kph,
+      wind_direction: data.current.wind_dir,
+      humidity: data.current.humidity,
+      cloud: data.current.cloud
+    },
+  };
+
+  return cityProcessedData;
+}
+
+function searchCity(search_input) {
+  getWeatherData(search_input).then(data => {
+    if (data === undefined) throw new Error('City not found');
+    weatherInfoDOM.displayWeatherData(data);
+  }).catch(error => alert(error));
+}
+
+export {
+  processData,
+  searchCity
+};
