6e31ed50d98553bb694c21e9604ebeada2f25fb1
ivandnd <121280104+ivandnd@users.noreply.github.com>
2024-02-02 18:12:36 +0800

    Change api to fetch weekly data

--- src/modules/WeatherApi.js
+++ src/modules/WeatherApi.js
@@ -4,7 +4,7 @@ const API_KEY = 'e87308b858e24e43ab0135521243001';
 
 async function getWeatherData(city) {
   try {
-    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
+    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`);
     if (!response.ok || response.status !== 200) throw new Error('Failed to fetch data');
     return processData(await response.json());
   } catch(error) {
