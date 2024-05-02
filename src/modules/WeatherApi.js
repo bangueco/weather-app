0e108f8525e58255166d7ea0a8d18bad43bf84de
bangueco <121280104+bangueco@users.noreply.github.com>
2024-05-02 20:18:13 +0800

    Update fetching

--- src/modules/WeatherApi.js
+++ src/modules/WeatherApi.js
@@ -4,7 +4,7 @@ const API_KEY = 'a75846f36a39400e9d132349240105';
 
 async function getWeatherData(city) {
   try {
-    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`);
+    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&aqi=no`);
     if (!response.ok || response.status !== 200) throw new Error('Failed to fetch data');
     return processData(await response.json());
   } catch(error) {
