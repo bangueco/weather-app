7649f72dead79f90f5ce74157567547c544d92f0
bangueco <121280104+bangueco@users.noreply.github.com>
2024-05-03 20:29:11 +0800

    Update current forecast

--- src/modules/components/CurrentForecast.js
+++ src/modules/components/CurrentForecast.js
@@ -1,20 +1,16 @@
 const CurrentForecast = (data) => {
 
   // Create dom elements
-  const conditionIcon = document.querySelector('.conditionIcon');
-  const conditionStatus = document.querySelector('.conditionStatus');
   const temperature = document.querySelector('.temperature');
   const windkph = document.querySelector('.wind');
   const humidity = document.querySelector('.humidity');
   const feelsLike = document.querySelector('.feelsLike');
 
   // Adding data per elements
-  temperature.textContent = `${data.current_forecast.temperature} °C`;
-  conditionIcon.src = `https:${data.current_forecast.condition.icon}`;
-  conditionStatus.textContent = data.current_forecast.condition.condition_status;
+  temperature.innerHTML = `${data.current_forecast.temperature}<span class="tempsymbol">°C</span>`;
   windkph.textContent = `Wind: ${data.current_forecast.wind}kph`;
   humidity.textContent = `Humidity: ${data.current_forecast.humidity}%`;
-  feelsLike.textContent = `Feels Like: ${data.current_forecast.feels_like}`;
+  feelsLike.innerHTML = `Feels Like: ${data.current_forecast.feels_like}<span class="tempsymbol2">°C</span>`;
 };
 
 export default CurrentForecast;
